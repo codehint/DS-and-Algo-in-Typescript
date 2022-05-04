fe1c13f784e8eacad82dc49d478c7431267d56ae
codehint <karnal.ahmad66@gmail.com>
2022-05-04 22:45:37 +0500

    test (Linked List): added more test calls.

--- Data_Structures/src/Linked-List/test.ts
+++ Data_Structures/src/Linked-List/test.ts
@@ -3,7 +3,20 @@ import { LinkedList } from "./linked-list";
 const list = new LinkedList(4);
 list.addNode(5);
 list.addNode(10);
+
+// Print all elements
 list.printList();
+
+// Delete node by value
 list.deleteNodeByValue(4);
 list.printList();
+
+// Find by value
 list.findNodeByValue(4);
+
+// Get length of the list
+console.log(list.length);
+
+// Delete node by position
+list.deleteNodeByPosition(2);
+list.printList();
